561cb0f55c285ef1ed0bcd5133a72dabbf227fa3
xiaoming <1244783871@qq.com>
2023-03-18 20:12:35 +0800

    fix

--- main.py
+++ main.py
@@ -114,8 +114,8 @@ async def init_task(task_type: str, destination_type: str, task_name: str, prior
 
 
 # 接受数据并处理
-@app.get("/task/process/{task_id}/{node_id}/{image_num}")
-async def task_process(task_id: str, node_id: str, image_num: int):
+@app.get("/task/process/{task_id}/{node_id}/{image_num}/{node_num}")
+async def task_process(task_id: str, node_id: str, image_num: int, node_num: int):
     # 如果任务id在tasks字典中，则更新任务状态
     if task_id in _tasks:
         _tasks[task_id][node_id]['task_status'] = 'processing'
@@ -124,7 +124,7 @@ async def task_process(task_id: str, node_id: str, image_num: int):
         if node_id == 'edge':
             # 模拟处理数据,0.5秒
             time.sleep(0.5)
-            _tasks[task_id][node_id]['task_progress'] = int(image_num) / 500
+            _tasks[task_id][node_id]['task_progress'] = int(image_num) / node_num*125
             logger.info(f"data processing, task id: {task_id}, task_node: {node_id}, "
                         f"progress: {_tasks[task_id][node_id]['task_progress']}")
         else:
