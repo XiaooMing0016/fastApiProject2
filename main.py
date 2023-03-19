0a08f4050393656a65793c2f1ffc3a49f753e467
xiaoming <1244783871@qq.com>
2023-03-19 13:06:33 +0800

    fix

--- main.py
+++ main.py
@@ -76,9 +76,9 @@ async def init_task(task_type: str, destination_type: str, task_name: str, prior
                         f"Created {task_type_name} task {task_name}:{task_id} for {destination_type} node {str(i)} "
                         f"is failed, priority: {priority}, response: ")
             except Exception as e:
-                logger.error(
+                logger.info(
                     f"Created {task_type_name} task {task_name}:{task_id} for {destination_type} node {str(i)} "
-                    f"is error, priority: {priority}, error: {e}")
+                    f"is error, priority: {priority}, {e}")
     elif destination_type == 'edge':
         try:
             logger.info(
